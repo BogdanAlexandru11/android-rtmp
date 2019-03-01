8739158e956f8af9888a2e95b2ffb1aa43e1b29a
alex <abcbogdan11@gmail.com>
2019-03-01 14:59:18 +0000

    default stream

---
+++ app/src/androidTest/java/com/pedro/rtpstreamer/MainActivityTest.java
@@ -0,0 +1,30 @@
+package com.pedro.rtpstreamer;
+
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.CAMERA",
+                    "android.permission.RECORD_AUDIO",
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+
+    @Test
+    public void mainActivityTest() {
+    }
+}
